e169c0b4139ed394b5e683e049e09f7a9fe222a6
Roberto Ivan Aguirre Silerio <ext_riaguirres@falabella.com.mx>
2024-02-20 16:38:28 -0600

    Hecho la estructura de la pagina html y poco de Css, nada de JS

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-const switchBoton = document.getElementById("cambio")
-switchBoton.setAttribute("value","Encriptar -> Descencriptar")
-
-switchBoton.addEventListener("click", function () {
-    switch(switchBoton.getAttribute("value")){
-        case "Encriptar -> Descencriptar": switchBoton.setAttribute("value","Desencriptar -> Encriptar"); break;
-        case "Desencriptar -> Encriptar": switchBoton.setAttribute("value","Encriptar -> Descencriptar"); break;
-    }
-    console.log(switchBoton.getAttribute("value"));
-
-});
-
 
 
 
