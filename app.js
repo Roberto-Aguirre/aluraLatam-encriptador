52c25d252749f8171bef81eae45338026ad35944
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-02-21 17:45:11 -0600

    Acabe el front end con CSS y el html, se esta trabajando en Js con los objetos

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+const inicial = document.getElementById("area-resultado");
+const bloqueResultado = document.getElementById("resultado");
+console.log(inicial);
+
+const encriptar = document.getElementById("encriptar");
+const desencriptar = document.getElementById("desencriptar");
+
+encriptar.addEventListener("click",);
 
 
+function validarTexto() {
+    
 
+
+}
+
+let texto = "";
+
+function encriptar(texto){
+
+}
 
 
