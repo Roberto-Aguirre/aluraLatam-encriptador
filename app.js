a93605507a1c1657cf0fb6aff5f0ede7e5909cb1
Roberto Ivan Aguirre Silerio <ext_riaguirres@falabella.com.mx>
2024-02-22 22:49:23 -0600

    Final del front end para desktop y funcion de encriptar al 50%

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
 const inicial = document.getElementById("area-resultado");
 const bloqueResultado = document.getElementById("resultado");
 console.log(inicial);
+const textarea = document.getElementById("texto");
+const encriptarBtn = document.getElementById("encriptar");
+const desencriptarBtn = document.getElementById("desencriptar");
 
-const encriptar = document.getElementById("encriptar");
-const desencriptar = document.getElementById("desencriptar");
-
-encriptar.addEventListener("click",);
+let frase = "";
 
 
-function validarTexto() {
+/**
+ * Reglas
+ * Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+    // La letra "e" es convertida para "enter"
+    // La letra "i" es convertida para "imes"
+    // La letra "a" es convertida para "ai"
+    // La letra "o" es convertida para "ober"
+    // La letra "u" es convertida para "ufat"
+
+*/
+
+
+let fraseArray = [];
+
+function traerTexto() {
+    let frase = textarea.innerText;
+    return frase;
+}
+
+function encriptar(frase,fraseArray){
+    for (let index = 0; index < frase.length; index++) {
+        let letra = frase[index];
     
-
-
+        switch (letra) {
+            case 'e':
+                letra = "enter";
+                break;
+            case 'i':
+                letra = "imes";
+                break;
+            case 'a':
+                letra = "ai";
+                break;
+            case 'o':
+                letra = "ober";
+                break;
+            case 'u':
+                letra = "ufat";
+                break;
+            default:
+                break;
+        }
+        fraseArray.push(letra);
+    }
+    return fraseArray;
 }
 
-let texto = "";
+encriptar(frase,fraseArray);
 
-function encriptar(texto){
-
-}
+console.log(fraseArray);
+console.log(frase[0]);
 
 
